41cc8a638ddc133a4c2273209f043453ce3a4e00
huyduc2712git <huyduc2712.git@gmail.com>
2026-08-18 10:33:10 +0700

    fix(android): remove ndkVersion and accept android-sdk-licenses

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 android {
     namespace = "com.finly.app.finly"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
